2c4fc7ea25a4d6666953d63b2f56da05a38e6219
Ralph Huwiler <ralph@huwiler.rocks>
2016-04-28 17:46:37 +0200

    fix typo

--- Resources/lang/en/activities.php
+++ Resources/lang/en/activities.php
@@ -2,9 +2,9 @@
 
 return [
     'created_object' => [
-        'summary' => '{{:user}} created a page, <a href=":url">:title</a>',
+        'summary' => ':user created a page, <a href=":url">:title</a>',
     ],
     'updated_object' => [
-        'summary' => '{{:user}} updated a page, <a href=":url">:title</a>',
+        'summary' => ':user updated a page, <a href=":url">:title</a>',
     ],
 ];
